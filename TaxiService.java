4dfa03f90218e6bc2d768afb65ba1c0a7b1b6967
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2023-08-21 15:00:00 -0400

    Update TaxiService.java with exception handling

--- TaxiService.java
+++ TaxiService.java
@@ -2,6 +2,33 @@ package oop_driver_taxi;
 
 public class TaxiService 
 {
+	public void validate(Ride ride) throws InvalidRideException
+	{
+		if(ride.getDriver() == null || ride.getPassengers() == null)
+		{
+			throw new InvalidRideException();
+		}
+		if(ride.getPassengers().length > 4 || ride.getPassengers().length < 1)
+		{
+			throw new InvalidRideException();
+		}
+	}
+	
+	public int business(Ride ride)
+	{
+		int count = 0;
+		Passenger[] passengers = ride.getPassengers();
+		
+		for(Passenger passenger : passengers)
+		{
+			if(passenger.getAccountType()=="BUSINESS")
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
 	public Ride process(Driver driver, Passenger[] passengers)
 	{
 		Ride ride = new Ride(driver, passengers);
